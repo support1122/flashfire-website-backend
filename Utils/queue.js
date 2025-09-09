94ee9e5afaf4f53cf1bb7d1124eaa14e8d1a1f05
Biswajit Sharma <biswajit@Biswajit.>
2025-09-09 19:01:24 +0000

    rfv

--- Utils/queue.js
+++ Utils/queue.js
@@ -3,9 +3,9 @@ import IORedis from 'ioredis';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const connection = new IORedis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-});
+// const connection = new IORedis({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT
+// });
 
 export const callQueue = new Queue('callQueue', { connection: {url: process.env.UPSTASH_REDIS_URL} });
