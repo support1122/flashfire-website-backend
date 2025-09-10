071abda9b4dada8db15baffcc027a096a54a91ed
Biswajit Sharma <biswajit@Biswajit.>
2025-09-10 10:29:18 +0000

    f

--- Controllers/TwilioReminder.js
+++ Controllers/TwilioReminder.js
@@ -1,18 +1,25 @@
 export default async function TwilioReminder(req, res) {
-  console.log('twilio reminder hitt..')
-  const meetingTime = req.query.meetingTime || 'your scheduled time';
-  const role = req.query.role || 'participant';
-  const twiml = new Twilio.twiml.VoiceResponse();
+  try{
+    console.log('twilio reminder hitt..');
+    const meetingTime = req.query.meetingTime || 'your scheduled time';
+    const role = req.query.role || 'participant';
+    const twiml = new Twilio.twiml.VoiceResponse();
 
-  const gather = twiml.gather({
-    numDigits: 1,
-    action: '/twilio/response',
-    method: 'POST'
-  });
+    const gather = twiml.gather({
+      numDigits: 1,
+      action: '/twilio/response',
+      method: 'POST'
+    });
 
-  gather.say(`Hello! This is a reminder for your meeting scheduled with Flashfire at ${meetingTime}.`);
+    gather.say(`Hello! This is a reminder for your meeting scheduled with Flashfire at ${meetingTime}.`);
 
-  twiml.say('Thank you. Goodbye.');
-  res.type('text/xml');
-  res.send(twiml.toString());
+    twiml.say('Thank you. Goodbye.');
+    res.type('text/xml');
+    res.send(twiml.toString());
+
+  }
+  catch(error){
+    console.log('twilio error,', error)
+  }
+  
 }
