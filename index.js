fbaa79729e0ffa6c95b6364c6e50be1083365730
Biswajit Sharma <biswajit@Biswajit.>
2025-09-19 03:29:38 +0000

    frc

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
 );
 
 // ✅ Handle preflight requests for all routes
-app.options("*", cors());
+// app.options("*", cors());
 // app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
